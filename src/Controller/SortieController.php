32aeb5f583390ba4b9f96dd4100eac0a62b7d675
Mathis <91868032+MatekMathis@users.noreply.github.com>
2024-01-31 16:37:54 +0100

    feat:2001-2007-realisation_des_filtres_des_sorties_et_archivage_sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,31 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/sortie')]
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function index(SiteRepository $siteRepository, SortieRepository $sortieRepository, ParticipantRepository $participantRepository): Response
+    #[Route('', name: 'app_sortie')]
+    public function index(Request $request, SiteRepository $siteRepository, SortieRepository $sortieRepository, ParticipantRepository $participantRepository): Response
     {
-        $User = $participantRepository->find(5);
-
-        $Today = time();
-
+        $user = $participantRepository->find(5);
+        $today = time();
         $sites = $siteRepository->findAll();
-        $sorties = $sortieRepository->findAll();
+
+        $cb1 = $request->query->get('cb1', false);
+        $cb2 = $request->query->get('cb2', false);
+        $cb3 = $request->query->get('cb3', false);
+        $cb4 = $request->query->get('cb4', false);
+        $site = $request->query->get('site', 'All');
+        $motclef = $request->query->get('motclef', '');
+        $dateDebut = $request->query->get('dateDebut', null);
+        $dateFin = $request->query->get('dateFin', null);
+
+        $sorties = $sortieRepository->findWithFilters($user, $cb1, $cb2, $cb3, $cb4, $site,$motclef, $dateDebut, $dateFin);
+
 
         return $this->render('sortie/index.html.twig', [
             'controller_name' => 'SortieController',
             'sites' => $sites,
             'sorties' => $sorties,
-            'today' => $Today,
-            'user' => $User,
+            'today' => $today,
+            'user' => $user,
         ]);
     }
+
 }
