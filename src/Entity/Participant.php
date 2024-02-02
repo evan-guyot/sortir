d98602fe0d03dc452f9619cacb8e238c4bcc5b2f
Mehdi <130163591+medoxx212@users.noreply.github.com>
2024-02-02 16:26:01 +0100

    Revert "fieat:1003:modifierMonProfil"

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,17 +6,10 @@ use App\Repository\ParticipantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
-/**
- * @ORM\Entity(repositoryClass="App\Repository\ParticipantRepository")
- * @UniqueEntity(fields={"pseudo"}, message="Ce pseudo est déjà utilisé.")
- */
 
 #[ORM\Entity(repositoryClass: ParticipantRepository::class)]
-class Participant implements UserInterface, PasswordAuthenticatedUserInterface
+class Participant implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -275,10 +268,5 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
         }
 
         return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->motdepasse;
     }
 }
