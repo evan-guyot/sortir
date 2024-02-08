08012cf9708e99c028254df2985544556dffbaa4
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-02-08 15:37:04 +0100

    fix:x implementation-bd-pour-test

---
+++ tests/ParticipantTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Entity\Participant;
+use App\Entity\Site;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ParticipantTest extends KernelTestCase
+{
+    public function testAjoutParticipant() : void{
+
+        self::bootKernel();
+
+        $entityManager = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $site = new Site();
+        $site->setNom("Nantes");
+
+        $entityManager->persist($site);
+        $entityManager->flush();
+
+        $participant = new Participant();
+        $participant->setPseudo('PseudoParticipant');
+        $participant->setNom('NomParticipant');
+        $participant->setPrenom('PrenomParticipant');
+        $participant->setMail('participant@test.com');
+        $participant->setActif(true);
+        $participant->setAdministrateur(true);
+        $participant->setMotdepasse("fakePassword");
+        $participant->setSite($site);
+
+        $entityManager->persist($participant);
+        $entityManager->flush();
+
+        $insertedProduct = $entityManager->getRepository(Participant::class)->findOneBy([
+            'pseudo' => 'PseudoParticipant',
+        ]);
+
+        self::assertNotNull($insertedProduct);
+        self::assertEquals('NomParticipant', $insertedProduct->getNom());
+        self::assertEquals('PrenomParticipant', $insertedProduct->getPrenom());
+    }
+}
